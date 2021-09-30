c4fd6fc90a3f381c6dd5087bfecacf05eb62f154
Kim Dong In <needshield@gmail.com>
2021-09-30 17:58:21 +0900

    modify DumbJob for testing field injection with setters

--- src/main/java/job/DumbJob.java
+++ src/main/java/job/DumbJob.java
@@ -10,6 +10,10 @@ import org.quartz.JobKey;
 
 public class DumbJob implements Job {
 
+  String jobSays;
+  float myFloatValue;
+  ArrayList<Date> myStateData;
+
   @Override
   public void execute(JobExecutionContext context) throws JobExecutionException {
     System.out.println("Tutorial Lesson 3 - Hello DumbJob");
@@ -19,15 +23,22 @@ public class DumbJob implements Job {
     // JobDetail과 Trigger의 JobDataMap을 병합한 형태의 객체를 가져옴
     JobDataMap dataMap = context.getMergedJobDataMap();
 
-    String jobSays = dataMap.getString("jobSays");
-    float myFloatValue = dataMap.getFloat("myFloatValue");
-
-    // Trigger에 입력했던 myStateData 데이터를 불러와서 본래 타입에 맞게 캐스팅
-    ArrayList<Date> state = (ArrayList<Date>) dataMap.get("myStateData");
-    state.add(new Date());
+    myStateData.add(new Date());
 
     System.out.println(
         "Instance " + key + " of DumbJob says: " + jobSays + ", and val is: " + myFloatValue
-            + ", state: " + state);
+            + ", state: " + myStateData);
+  }
+
+  public void setJobSays(String jobSays) {
+    this.jobSays = jobSays;
+  }
+
+  public void setMyFloatValue(float myFloatValue) {
+    this.myFloatValue = myFloatValue;
+  }
+
+  public void setMyStateData(ArrayList<Date> myStateData) {
+    this.myStateData = myStateData;
   }
 }
